d15f1e74093bbcbf286355d2fc90e28a913ea7bf
Arielton97 <arieltonribeiro97@gmail.com>
2024-04-15 21:37:30 -0300

    quarto-commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Vambora");
+        String meuNome = "Arielton ";
+        String meuSobrenome = "Ribeiro ";
+        int somaIdade = 20;
+        int minhaIdade = 7;
+        int idade = somaIdade + minhaIdade;
+        String nomeInicialAtleta = "Cristiano Ronaldo";
+        String nomeMeioAtleta = "Aveiro dos Santos";
+
+        
+        String nomeCompleto = nomeCompleto(nomeInicialAtleta, nomeMeioAtleta);
+        System.out.println(nomeCompleto);
     }
+
+    public static String nomeCompleto (String meuNome, String meuSobrenome) {
+        return "Resultado do metodo " + meuNome.concat(" ").concat(meuSobrenome);
+    }
+        
 }
